4e28127c0e3ad8845a71e2d411af924f4d748265
User1 <sevenmuratt@gmail.com>
2023-06-19 23:26:36 +0300

    day16-2

--- basics/src/components/25-formik-yup/02-formik-yup.js
+++ basics/src/components/25-formik-yup/02-formik-yup.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Container, Form } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
+import axios from 'axios';
 
 const FormikYup2 = () => {
 
@@ -10,6 +11,7 @@ const FormikYup2 = () => {
         lastName: "",
         email: "",
         password: "",
+        id: ""
     };
 
     const validationSchema = yup.object({
@@ -28,8 +30,23 @@ const FormikYup2 = () => {
             .required("Sifre kismini bos birakamazsiniz!"),
     });
 
-    const onSubmit = (values) => {
-        console.log("Form data:", values)
+    const onSubmit = async (values) => {
+
+        const dto = {
+            firstName: values.firstName,
+            lastName: values.lastName,
+            email: values.email,
+            password: values.password
+        }
+
+
+        try {
+            const resp = await axios.put(`https://648a1ac75fa58521cab0d225.mockapi.io/api/v1/login/${values.id}`, values);
+            console.log(resp)
+
+        } catch (error) {
+            console.log(error.message)
+        }
     };
 
     const formik = useFormik({
@@ -38,12 +55,20 @@ const FormikYup2 = () => {
         onSubmit: onSubmit
     })
 
-
     return (
         <Container>
             <h2>Formik & Yup — 2</h2>
             <p>*Doldurulmasi zorunlu alanlar.</p>
-            <Form onSubmit={formik.handleSubmit}>
+            <Form noValidate onSubmit={formik.handleSubmit}>
+                <Form.Group className="mb-3">
+                    <Form.Label>ID</Form.Label>
+                    <Form.Control
+                        type="number"
+                        name="id"
+                        placeholder="Lutfen id giriniz..."
+                        {...formik.getFieldProps("id")}
+                    />
+                </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>First Name*</Form.Label>
                     <Form.Control
@@ -60,8 +85,61 @@ const FormikYup2 = () => {
                         {formik.errors.firstName}
                     </Form.Control.Feedback>
                 </Form.Group>
+                {/* ------------------------ */}
+                <Form.Group className="mb-3">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control
+                        type='text'
+                        name='lastName'
+                        placeholder="Lutfen soyisminizi giriniz..."
+                        value={formik.values.lastName}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        isValid={formik.touched.lastName && !formik.errors.lastName}
+                        isInvalid={formik.touched.lastName && formik.errors.lastName}
+                    />
+                    <Form.Control.Feedback type='invalid'>
+                        {formik.errors.lastName}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                {/* ------------------------ */}
+                <Form.Group className="mb-3">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type='email'
+                        name='email'
+                        placeholder="Lutfen soyisminizi giriniz..."
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        isValid={formik.touched.email && !formik.errors.email}
+                        isInvalid={formik.touched.email && formik.errors.email}
+                    />
+                    <Form.Control.Feedback type='invalid'>
+                        {formik.errors.email}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                {/* ------------------------ */}
+                <Form.Group className="mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type='text'
+                        name='password'
+                        placeholder="Lutfen soyisminizi giriniz..."
+                        {...formik.getFieldProps("password")}
+                        isValid={formik.touched.password && !formik.errors.password}
+                        isInvalid={formik.touched.password && formik.errors.password}
+                    />
+                    <Form.Control.Feedback type='invalid'>
+                        {formik.errors.password}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Button type='submit'>
+                    FORMU GONDER
+                </Button>
             </Form>
         </Container>
     )
 }
+
 export default FormikYup2
